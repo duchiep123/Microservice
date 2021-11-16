5b1576973c74096791a0efed5a3d0cd92da4eed8
Hiep Nguyen Duc <mrhiep314@gmail.com>
2021-11-16 16:13:36 +0700

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Start 
+docker-compose build
+docker-compose up
+
+Create sqs, sns subscribing sqs to sns and config dead letter queue
+sh create-sqs-sns.shs
--- create-sqs-sns.sh
+++ create-sqs-sns.sh
@@ -1,12 +1,7 @@
 
-queue_name="first-queue"
-topic_name="first-topic"
-dlq_queue="${queue_name}-dlq"
-
-
-
 aws --endpoint-url=http://localhost:4566 sns create-topic --name first-topic
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name first-queue
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name first-queue-dlq
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:us-east-1:000000000000:first-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:first-queue
 aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url http://localhost:4566/000000000000/first-queue --attributes file://set-queue-attributes.json
+
